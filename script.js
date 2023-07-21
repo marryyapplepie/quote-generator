8576f8fcfd17c5646125d2b58b7b4f4bbcbc1271
Maria Stenina <marryyapplepie@outlook.com>
2023-07-21 20:31:54 +0300

    add github link

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ function generate() {
     var quotes = { 
         "- Hitch -" : '“Life is not the amount of breaths you take. It is the moments that take your breath away.”',
         "-  The Secret Life of Walter Mitty -" : '“To see the world, things dangerous to come to, to see behind walls, to draw closer, to find each other and to feel. That is the purpose of life.”',
-       "- 10 Things I Hate About You -": '“Don`t let anyone ever make you feel like you don`t deserve what you want.”'
+        "- 10 Things I Hate About You -": '“Don`t let anyone ever make you feel like you don`t deserve what you want.”',
+        "- Dead Poets Society -" : '"Carpe diem. Seize the day, boys. Make your lives extraordinary"'
     }
+
     var movies = Object.keys(quotes);
+    
     var movie = movies[Math.floor(Math.random() * movies.length)];
+
     var quote = quotes[movie];
     document.getElementById("quote").innerHTML = quote;
     document.getElementById("movie").innerHTML = movie;
